d8cdad937d84ae790b75fe31e47c92ef0913a577
alinexon <alinexon55@gmail.com>
2026-07-02 22:43:09 +0430

    change contact form design according to the new design figma

--- components/wow/shared/ContactDialogProvider.tsx
+++ components/wow/shared/ContactDialogProvider.tsx
@@ -3,8 +3,6 @@
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
-  DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog'
 import { useLenis } from 'lenis/react'
@@ -80,21 +78,13 @@ export function ContactDialogProvider({ children }: { children: ReactNode }) {
         <DialogContent
           className="flex max-h-[min(90vh,calc(100dvh-2rem))] flex-col gap-0 overflow-hidden rounded-radius-md border border-[#1515151A] bg-backgroundBody p-0 shadow-sm ring-0 dark:border-[#EDF0F51A] dark:bg-dark dark:shadow-none sm:max-w-[860px]"
         >
-          <DialogHeader className="shrink-0 gap-2 p-6 pb-4 sm:p-8 sm:pb-5">
-            <DialogTitle className="text-2xl font-semibold text-secondary dark:text-backgroundBody">
-              Get in Touch
-            </DialogTitle>
-            <DialogDescription className="text-colorText dark:text-dark-100">
-              Tell us about your project and we&apos;ll get back to you shortly.
-            </DialogDescription>
-          </DialogHeader>
-
           <div
             ref={scrollContainerRef}
             data-contact-dialog-scroll
             data-lenis-prevent
-            className="min-h-0 flex-1 overflow-y-auto overscroll-contain px-6 pb-6 sm:px-8 sm:pb-8 [-webkit-overflow-scrolling:touch]"
+            className="min-h-0 flex-1 overflow-y-auto overscroll-contain p-6 sm:p-8 [-webkit-overflow-scrolling:touch]"
           >
+            <DialogTitle className="sr-only">Let&apos;s Discuss Your Project</DialogTitle>
             <WowContactForm onSubmitted={handleSubmitted} />
           </div>
         </DialogContent>
