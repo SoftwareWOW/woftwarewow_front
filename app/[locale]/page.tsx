57e1cf0a94289709d30c3fd83ae0a35ca0dccce5
alinexon <alinexon55@gmail.com>
2026-07-05 20:05:30 +0430

    reducse marge between section

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -37,10 +37,12 @@ const Home = async ({ params }: Props) => {
 
   return (
     <WowLayout>
-      <div className="flex flex-col gap-12 pb-12 sm:gap-16 sm:pb-16 md:gap-24 md:pb-24 lg:gap-32 lg:pb-32 xl:gap-40 xl:pb-40 2xl:gap-[200px] 2xl:pb-[200px]">
+      <div className="flex flex-col gap-12 sm:gap-16 md:gap-24 lg:gap-32 xl:gap-40 2xl:gap-[200px]">
         <WowHero hero={dictionary.hero} />
-        <WowEcosystem ecosystem={dictionary.ecosystem} />
+     <div className="flex flex-col gap-0 lg:contents">
+         <WowEcosystem ecosystem={dictionary.ecosystem} />
         <WowSuperAgencyClient superAgencyClient={dictionary.superAgencyClient} />
+     </div>
         <Stats />
         <DevisionOverview />
         <SolutionToChallengesSection />
@@ -48,7 +50,9 @@ const Home = async ({ params }: Props) => {
         <HumanTuch />
         <Faq />
         <GrowthStrategies />
-        <WowGrowthCta />
+        <div className="mb-3">
+          <WowGrowthCta />
+        </div>
       </div>
     </WowLayout>
   )
