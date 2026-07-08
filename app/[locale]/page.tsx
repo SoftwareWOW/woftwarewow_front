3577f4fd4b70cad65392a37a5bc7b22fc8bb0fe2
alinexon <alinexon55@gmail.com>
2026-07-08 20:25:35 +0430

    change footer link text

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -41,7 +41,7 @@ const Home = async ({ params }: Props) => {
   return (
     <WowLayout>
       <div className="flex flex-col gap-12 sm:gap-16 md:gap-24 lg:gap-32 xl:gap-40 2xl:gap-[200px]">
-        <WowHero hero={dictionary.hero} />
+        {/* <WowHero hero={dictionary.hero} />
      <div className="flex flex-col gap-0 lg:contents">
          <WowEcosystem ecosystem={dictionary.ecosystem} />
         <WowSuperAgencyClient superAgencyClient={dictionary.superAgencyClient} />
@@ -53,7 +53,7 @@ const Home = async ({ params }: Props) => {
            <Marquee />
         <HumanTuch />
         <Faq />
-        <GrowthStrategies />
+        <GrowthStrategies /> */}
         <div className="mb-3">
           <WowGrowthCta />
         </div>
