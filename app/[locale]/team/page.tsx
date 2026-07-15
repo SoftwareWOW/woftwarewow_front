47dd4aa3358a94ed53b581fdc2712d3e4628f1b6
alinexon <alinexon55@gmail.com>
2026-07-15 16:13:43 +0430

    make components of team to its folder

--- app/[locale]/team/page.tsx
+++ app/[locale]/team/page.tsx
@@ -2,12 +2,14 @@ import Team from '@/components/aboutpage/Team'
 import CTA from '@/components/shared/CTA'
 import CtaImageSlider from '@/components/shared/CtaImageSlider'
 import LayoutOne from '@/components/shared/LayoutOne'
-import PageHero from '@/components/shared/PageHero'
+
+import WowGrowthCta from '@/components/wow/LandascapComponets/WowGrowthCta'
+import TeamHero from './_components/TeamHero'
 
 const TeamPage = () => {
   return (
     <LayoutOne>
-      <PageHero
+      <TeamHero
         title="Our Creative "
         italicTitle="Team"
         badgeTitle="Team"
@@ -17,18 +19,7 @@ to introduce your creative team. Let me know if you'd like any specific adjustme
         spacing="pt-32 md:pt-36 lg:pt-[180px] pb-10 sm:pb-14 md:pb-20 lg:pb-24 2xl:pb-[120px] overflow-hidden relative"
       />
       <Team />
-      <CTA>
-        Let's chat!
-        <CtaImageSlider
-          slides={[
-            { id: '1', img: '/images/agent/13.png' },
-            { id: '2', img: '/images/agent/17.png' },
-            { id: '3', img: '/images/agent/18.png' },
-          ]}
-        />
-        with us.
-        <i className="block font-instrument italic max-md:inline-block max-sm:pl-2 sm:mt-10">A virtual coffee?</i>
-      </CTA>{' '}
+ <WowGrowthCta />
     </LayoutOne>
   )
 }
