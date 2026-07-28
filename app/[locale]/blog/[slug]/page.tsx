9b2ad7d9018df8e52c5fcebf864751b9e59a2dd7
alinexon <alinexon55@gmail.com>
2026-07-28 15:52:33 +0430

    change blog details page

--- app/[locale]/blog/[slug]/page.tsx
+++ app/[locale]/blog/[slug]/page.tsx
@@ -6,6 +6,8 @@ import PageHero from '@/components/shared/PageHero'
 import getMarkDownContent from '@/utils/GetMarkDownContent'
 import getMarkDownData from '@/utils/GetMarkDownData'
 import { BlogType } from '../page'
+import WowGrowthCta from '@/components/wow/LandascapComponets/WowGrowthCta'
+import BlogDetailsHero from '../_components/BlogDetailsHero'
 
 export async function generateStaticParams() {
   const blogs: BlogType[] = getMarkDownData('data/blogsV2')
@@ -22,25 +24,14 @@ const BlogDetails = async ({ params }: { params: Promise<{ slug: string }> }) =>
 
   return (
     <LayoutOne>
-      <PageHero
+      <BlogDetailsHero
         badgeTitle="Blog Details"
         title={postBlog.title}
         description={postBlog.description}
         spacing="pt-32 md:pt-44 lg:pt-[200px] pb-10 md:pb-16 lg:pb-[88px] xl:pb-[100px] relative overflow-hidden"
       />
       <BlogContent blog={blog} />
-      <CTA>
-        Let's chat!
-        <CtaImageSlider
-          slides={[
-            { id: '1', img: '/images/agent/01.jpg' },
-            { id: '3', img: '/images/agent/03.jpg' },
-            { id: '2', img: '/images/agent/11.png' },
-          ]}
-        />
-        with us.
-        <i className="block font-instrument italic max-md:inline-block max-sm:pl-2 sm:mt-10">A virtual coffee?</i>
-      </CTA>
+      <WowGrowthCta />
     </LayoutOne>
   )
 }
---
+++ app/[locale]/blog/_components/BlogDetailsHero.tsx
@@ -0,0 +1,38 @@
+import RevealWrapper from "@/components/animation/RevealWrapper"
+import HeroGradientAnimation from "@/components/shared/HeroGradientAnimation"
+
+
+interface PropsType {
+  badgeTitle?: string
+  title: string
+  italicTitle?: string
+  description?: string
+  spacing?: string
+  scale?: boolean
+}
+
+const BlogDetailsHero = ({ badgeTitle, title, description, italicTitle, spacing, scale }: PropsType) => {
+  return (
+    <section className={`${spacing ?? 'relative overflow-hidden py-32 md:py-40 lg:py-[185px]'} `}>
+      <HeroGradientAnimation scale={scale} />
+
+      <div className="container">
+        <RevealWrapper className="text-center">
+          {badgeTitle && (
+            <div className="rv-badge">
+              <span className="rv-badge-text">{badgeTitle}</span>
+            </div>
+          )}
+          {title && (
+            <h1 className="mb-4 mt-3.5">
+              {title} <i className="font-instrument italic">{italicTitle}</i>
+            </h1>
+          )}
+          {description && <p className="text-appear mx-auto max-w-[470px] md:max-w-[750px]">{description}</p>}
+        </RevealWrapper>
+      </div>
+    </section>
+  )
+}
+
+export default BlogDetailsHero
