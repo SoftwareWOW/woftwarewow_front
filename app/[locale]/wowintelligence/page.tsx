93a69ab4f79146e9370ca174d98b39379a496d21
alinexon <alinexon55@gmail.com>
2026-08-04 00:09:59 +0430

    change order process

--- app/[locale]/wowintelligence/page.tsx
+++ app/[locale]/wowintelligence/page.tsx
@@ -3,6 +3,7 @@ import WowGrowthCta from '@/components/wow/LandascapComponets/WowGrowthCta'
 import SoftwareRfq from '@/components/wow/softwarewow/SoftwareRfq'
 import BusinessProblems from '@/components/wow/wowintelligence/BusinessProblems'
 import IntelligenceFlowHero from '@/components/wow/wowintelligence/IntelligenceFlowHero'
+import IntelligenceClients from '@/components/wow/wowintelligence/IntelligenceClients'
 import IntelligenceProcess from '@/components/wow/wowintelligence/IntelligenceProcess'
 import OrderProcess from '@/components/wow/wowintelligence/OrderProcess'
 import type { Locale } from '@/i18n/config'
@@ -35,8 +36,9 @@ export default async function WowIntelligencePage({ params }: Props) {
       <div className="flex flex-col gap-12 sm:gap-16 md:gap-24 lg:gap-32 xl:gap-40 2xl:gap-[200px]">
         <IntelligenceFlowHero />
         <BusinessProblems />
-        <IntelligenceProcess />
         <OrderProcess />
+        <IntelligenceProcess />
+        <IntelligenceClients />
         <SoftwareRfq />
         <div className="mb-3">
           <WowGrowthCta />
